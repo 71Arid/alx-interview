5774b451ef17dfd669907e6e2216f3e945b54f70
71Arid <no232el@gmail.com>
2024-03-08 22:36:37 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def pascal_triangle(n):
+    matrix = [
+        [1],
+        [1, 1]
+    ]
+    if n == 1:
+        matrix = [[1]]
+        return matrix
+    elif n == 2:
+        return matrix
+    else:
+        i = 3
+        while i <= n:
+            new_row = []
+            prev_row = matrix[i-2]
+            new_column = len(prev_row)
+            new_row.append(1)
+            for j in range(1, new_column):
+                new_row.append(prev_row[j-1] + prev_row[j])
+            new_row.append(1)
+            matrix.append(new_row)
+            i = i + 1
+        return matrix
