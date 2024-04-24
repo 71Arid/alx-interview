520e33e74ef8e7f60ec5928d3302b2582b2fe28b
71Arid <no232el@gmail.com>
2024-04-24 18:32:33 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-f_id = process.argv[2];
+const Fid = process.argv[2];
 
-let url = `https://swapi-api.alx-tools.com/api/films/${f_id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${Fid}`;
 
 function fetchMovie(url) {
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            const film = JSON.parse(body);
-            const characters = film.characters;
-            characters.forEach(url => fetchCharacter((url)));
-        }
-    });
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const film = JSON.parse(body);
+      const characters = film.characters;
+      characters.forEach(url => fetchCharacter((url)));
+    }
+  });
 }
 
 function fetchCharacter(url) {
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            const character = JSON.parse(body);
-            console.log(character.name);
-        }
-    });
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const character = JSON.parse(body);
+      console.log(character.name);
+    }
+  });
 }
 
 fetchMovie(url);
